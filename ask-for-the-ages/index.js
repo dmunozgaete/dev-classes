00b1c0f1de4d2a7ad3d24087147bfbd07e19407c
Peanut Hub <peanuthub@MBP-de-Peanut.iptv.microsoft.com>
2019-04-17 23:10:40 -0300

    we organized the questions, math, and results better

--- ask-for-the-ages/index.js
+++ ask-for-the-ages/index.js
@@ -2,25 +2,40 @@
 const chalk = require("chalk")
 const readline = require("readline")
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 const askToUser = (question)=>{
   return new Promise((resolve,reject)=>{
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-
-    rl.question(questionA, answer => {
+    rl.question(question, answer => {
         resolve(answer);
     });
   });
 };
 
-const showInConsole = ()=>{
+const clearConsole = ()=>{
+  console.clear();
+};
 
-}
-const answerA = await askToUser("what year were you born, David? ");
-const answerB = await askToUser("what year were you born, Hugo? ");
-const answerC = await askToUser("what year were you born, Izaak? ");
-const answerD = await askToUser("what year is it now? ");
+(async ()=>{
+  clearConsole();
+
+  const davidYearBorn = await askToUser("what year were you born, David? ");
+  const hugoYearBorn = await askToUser("what year were you born, Hugo? ");
+  const izaakYearBorn = await askToUser("what year were you born, Izaak? ");
+  const currentYear = new Date().getFullYear();
+
+  const resultA = currentYear - parseInt(davidYearBorn);
+  const resultB = currentYear - parseInt(hugoYearBorn);
+  const resultC = currentYear - parseInt(izaakYearBorn);
+  
+  console.log("David you are", resultA , "years old");
+  console.log("Hugo you are", resultB , "years old");
+  console.log("Izaack you are", resultC , "years old");
+
+})();
+
     
-console.log("You area" , result , "years ago");
+
